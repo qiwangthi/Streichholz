cd2b0d2d53126335e6ca73996d8dbc5de3fddd15
qiwangthi <164479452+qiwangthi@users.noreply.github.com>
2024-04-22 22:48:01 +0200

    22.April 22:00

--- StreichholzSpiel/src/Eingabe.java
+++ StreichholzSpiel/src/Eingabe.java
@@ -1,37 +1,27 @@
 import java.util.Scanner;
 
 public class Eingabe {
-    static int menschZug = 0; // Variable für die eingegebene Zahl
 
     public static int leseZahl() {
-
-        // LIEST EINE ZAHL VON DER KONSOLE EIN UND GIBT DIESE ALS INT-WERT ZURÜCK
-        Ausgabe.zahlEingeben(); // Ausgabe, dass eine Zahl eingegeben werden soll
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) { // Repeat until a valid number is entered
-                if (scanner.hasNextInt()) { // Check if the input is an integer
-                    menschZug = scanner.nextInt(); // Assign the input to the variable
-
-                    break; // Exit the loop
-                } else {
-                    Ausgabe.keineZahl(); // Ausgabe, dass keine Zahl eingegeben wurde
-                    scanner.nextLine(); // Clear the invalid input
-                }
-
-            }
+        Ausgabe.zahlEingeben();
+        Scanner scanner = new Scanner(System.in);
+        String eingabe = scanner.nextLine();
+        int menschZug = 0;
+        try {
+            menschZug = Integer.parseInt(eingabe);
+        } catch (NumberFormatException e) {
+            Ausgabe.keineZahl();
+            // menschZug = leseZahl();
         }
-
-        return menschZug; // Return the input as an integer
+        return menschZug;
     }
 
     public static int leseHoelzer() {
-        leseZahl();
-        if (menschZug != 1 && menschZug != 2 && menschZug != 3) {
+        int menschZug = leseZahl();
+        if (menschZug < 1 || menschZug > 3) {
             Ausgabe.zahlNichtImBereich();
-            return leseZahl();
-        } else {
-            return menschZug;
+            // menschZug = leseZahl();
         }
+        return menschZug;
     }
-
 }
--- StreichholzSpiel/src/Spiel.java
+++ StreichholzSpiel/src/Spiel.java
@@ -1,80 +1,75 @@
 public class Spiel {
     // Attribute
-    static int anzahl;
+    private int anzahl;
 
     // konstruktor
     public Spiel(int anzahl) {
-        Spiel.anzahl = anzahl;
+        this.anzahl = anzahl;
+    }
+
+    // getter
+    public int getAnzahl() {
+        return this.anzahl;
+    }
+
+    // setter
+    public void setAnzahl(int anzahl) {
+        this.anzahl = anzahl;
+    }
+
+    // die methode spielen soll das Spiel starten und abwechselnd die Methoden
+    // comuterZiehn und menschenZiehen aufrufen, solange sich noch steichhoelzer auf
+    // dem Tisch befinden. Wenn das Spiel beendet ist, soll die Methode den Gewinner
+    // ausgeben (Mensch oder Computer)
+    public void spielen() {
+        Ausgabe.spielStand(anzahl);
+        while (this.anzahl > 0) {
+            computerZiehen();
+            if (this.anzahl <= 0) {
+                Ausgabe.menschGewinnt();
+                break;
+            }
+            menschenZiehen();
+            if (this.anzahl <= 0) {
+                Ausgabe.computerGewinnt();
+                break;
+            }
+        }
     }
 
     // oeffentliche Methoden
     // im konstruktor soll nahc dem Aufruf der Mehode computerZiehen der Gewinn des
     // menschlichen spielers bekannt gegeben werden, sofern der Computer das letzte
     // Hloz nehmen musste
-    public static void computerZiehen() {
+    public void computerZiehen() {
         // Call the method to calculate the computer's move
 
         int computerZug = 0;
         computerZug = berechneComputerZug(computerZug);
         int gezogeneHoelzer = computerZug;
         anzahl -= gezogeneHoelzer;
-        Ausgabe.computerZug(computerZug, anzahl);
-        if (anzahl <= 0) {
-            Ausgabe.menschGewinnt();
-        }
+        Ausgabe.computerZug(computerZug, this.anzahl);
+
     }
 
-    public static void menschenZiehen() {
+    public void menschenZiehen() {
         int menschZug = Eingabe.leseHoelzer();
-        int gezogeneHoelzer = menschZug;
-        if (gezogeneHoelzer > anzahl) {
+
+        if (menschZug > this.anzahl) {
             Ausgabe.zugNichtMoeglich();
-            menschenZiehen();
+            // menschenZiehen();
+            Eingabe.leseHoelzer();
         } else {
-            anzahl -= gezogeneHoelzer;
+            anzahl -= menschZug;
             Ausgabe.menschZug(menschZug, anzahl);
-            if (anzahl <= 0) {
-                Ausgabe.computerGewinnt();
-            } else {
-                computerZiehen();
-            }
 
         }
     }
 
     // ziehen der Spieler und computer abwechselnd bis 1 Streichholz uebrig ist
 
-    public static void spielen() {
-
-        while (anzahl > 1) {
-            menschenZiehen();
-            if (anzahl == 1) {
-                Ausgabe.menschGewinnt();
-                break;
-            }
-            computerZiehen();
-            if (anzahl == 1) {
-                Ausgabe.computerGewinnt();
-                break;
-
-            }
-        }
+    // berechnet den Zug des Computers um das Spiel zu gewinnen,duerfen 1,2 oder 3
+    public int berechneComputerZug(int computerZug) {
+        return computerZug = this.anzahl % 3 + 1;
     }
-
-    // berechnet den Zug des Computers
-    public static int berechneComputerZug(int computerZug) {
-        computerZug = (int) (Math.random() * 3) + 1;
-        return computerZug;
-    }
-
-    // getter
-    public int getAnzahl() {
-        return Spiel.anzahl;
-    }
-
-    // setter
-    public void setStreichhoelzer(int anzahl) {
-        Spiel.anzahl = anzahl;
-    }
-
 }
